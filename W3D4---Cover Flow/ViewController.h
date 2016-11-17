09018b732879ccc5a75466e6c20885701aaff61f
guiworm <dylan.mccrindle@gmail.com>
2016-11-17 10:37:34 -0500

    Initial Commit

---
+++ W3D4---Cover Flow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W3D4---Cover Flow
+//
+//  Created by Dylan McCrindle on 2016-11-17.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ W3D4---Cover Flow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  W3D4---Cover Flow
+//
+//  Created by Dylan McCrindle on 2016-11-17.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
